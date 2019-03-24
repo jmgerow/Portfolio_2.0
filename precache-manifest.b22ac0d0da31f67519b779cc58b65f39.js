86b555d847160deae628f04578ec576a8c4db954
jmgerow <justingerow1@gmail.com>
2019-03-24 16:57:07 -0500

    Updates

---
+++ precache-manifest.b22ac0d0da31f67519b779cc58b65f39.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b719aeefdad3bbf52a5",
+    "url": "/Portfolio_2.0/static/css/main.cbac1f8e.chunk.css"
+  },
+  {
+    "revision": "4b719aeefdad3bbf52a5",
+    "url": "/Portfolio_2.0/static/js/main.fcad504f.chunk.js"
+  },
+  {
+    "revision": "7abb776e018fb4067503",
+    "url": "/Portfolio_2.0/static/js/runtime~main.6f2590eb.js"
+  },
+  {
+    "revision": "a392681333d0dc796a9f",
+    "url": "/Portfolio_2.0/static/js/2.d4fc6abd.chunk.js"
+  },
+  {
+    "revision": "518dc04365a5d53a97f58332b8037d31",
+    "url": "/Portfolio_2.0/static/media/chicago.518dc043.jpg"
+  },
+  {
+    "revision": "dd1553889685089c6ba0ec61fea52228",
+    "url": "/Portfolio_2.0/static/media/calsnap-logo-port.dd155388.png"
+  },
+  {
+    "revision": "9f24c7a83707353e6bfb1eda82105efe",
+    "url": "/Portfolio_2.0/static/media/waitER.9f24c7a8.PNG"
+  },
+  {
+    "revision": "0bfc93ae623796b3796c75e1327c6fed",
+    "url": "/Portfolio_2.0/static/media/brain.0bfc93ae.jpg"
+  },
+  {
+    "revision": "e738e29973999a6e0c4e9f7190810e45",
+    "url": "/Portfolio_2.0/static/media/article-scraper.e738e299.PNG"
+  },
+  {
+    "revision": "f4ad435732557399cd9a9fc95830b2d4",
+    "url": "/Portfolio_2.0/static/media/burger.f4ad4357.jpg"
+  },
+  {
+    "revision": "c2de98d3cdf026235a041d7007174e8c",
+    "url": "/Portfolio_2.0/static/media/friends.c2de98d3.jpg"
+  },
+  {
+    "revision": "49162f833902c5395c9c9cc2829cca9e",
+    "url": "/Portfolio_2.0/static/media/scrabble.49162f83.jpg"
+  },
+  {
+    "revision": "3ef8b144d7d31c4ee3e622403d18c194",
+    "url": "/Portfolio_2.0/static/media/shft.3ef8b144.PNG"
+  },
+  {
+    "revision": "b7142754b6d85f8ee8e4eab5f2841ef6",
+    "url": "/Portfolio_2.0/static/media/pyramid.b7142754.jpg"
+  },
+  {
+    "revision": "57fbd879ca873994e4afcae6fdc5d87b",
+    "url": "/Portfolio_2.0/static/media/train.57fbd879.jpg"
+  },
+  {
+    "revision": "73903c2161ad3bad2e9d7683dd1d7463",
+    "url": "/Portfolio_2.0/static/media/crystal.73903c21.jpg"
+  },
+  {
+    "revision": "6ea9fa6259735770ac589c761acda962",
+    "url": "/Portfolio_2.0/static/media/Justin_M_Gerow_-_Resume_v2_web.6ea9fa62.pdf"
+  },
+  {
+    "revision": "367926e3b0f25d3f6efd94dc609b1204",
+    "url": "/Portfolio_2.0/index.html"
+  }
+];
